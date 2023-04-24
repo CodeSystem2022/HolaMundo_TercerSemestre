9b596cbe11918bd3450cf969ef835dea613ae061
María Rosa Morales <69001260+mariarosa17@users.noreply.github.com>
2023-04-23 21:25:58 -0300

    Create funciones.js

---
+++ Javascript/Tercera semana/funciones.js
@@ -0,0 +1,10 @@
+//Morales, María Rosa
+
+//FUNCIONES
+
+function mi_Funcion(a,b){
+    return a+b; // devolverá la suma de a+b
+}
+
+let resultado = mi_Funcion(10,5) 
+console.log(resultado) // los argumentos son 10 y 5 por lo que imprimirá 15
