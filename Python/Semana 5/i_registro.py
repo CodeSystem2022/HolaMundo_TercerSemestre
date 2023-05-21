2b224fc3719d2fd17ad65d6ba04a6c640abcb1ff
María Rosa Morales <69001260+mariarosa17@users.noreply.github.com>
2023-05-21 14:55:08 -0300

    Create i_registro.py
    Inserción de 1 registro

---
+++ Python/Semana 5/i_registro.py
@@ -0,0 +1,18 @@
+import psycopg2  # Esto es para poder conectarnos a Postgresql
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'INSERT INTO persona (nombre, apellido, email) VALUE (%s, %s, %s)' #Placeholder
+            valores = ('Carlos', 'Lara', 'carlos_l@gmail.com')
+            cursor.execute(sentencia, valores) # De esta manera ejecutamos la sentencia
+            registros_insertados = cursor.rowcount
+            print ('Los registros insertados son: {registros_insertados}')
+
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    conexion.close()
+
+# https://www.psycopg.org/docs/usage.html
