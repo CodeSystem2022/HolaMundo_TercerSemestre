6547266d5d0cc46fdbe61c534b8cf45a11d79697
María Rosa Morales <69001260+mariarosa17@users.noreply.github.com>
2023-05-21 15:29:10 -0300

    carga de archivo actualizar_varios_registros.py con uso de update
    Update de varios registros en bd

---
+++ Python/Semana 5/actualizar_varios_registros.py
@@ -0,0 +1,19 @@
+import psycopg2  # Esto es para poder conectarnos a Postgresql
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_bd')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = (
+                ('Miguel', 'Yapur', 'm_yapur@mail.com', 4),
+                ('Rosa', 'Garcia', 'r_garcia@mail.com', 5)
+            )
+            cursor.executemany(sentencia, valores)  # De esta manera ejecutamos la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    conexion.close()
