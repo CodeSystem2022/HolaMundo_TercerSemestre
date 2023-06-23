66628115d295d4a7321f51f49a2b8efa8f313b84
María Rosa Morales <69001260+mariarosa17@users.noreply.github.com>
2023-06-23 14:10:47 -0300

    Agregado de la función seleccionarUsuario()

--- trabajo_final/database/operaciones.py
+++ trabajo_final/database/operaciones.py
@@ -17,3 +17,41 @@ def primeraFuncion(bd):
         print(tabla)
     # Cerramos la conexión
     bd.close()
+
+def segundaFuncion(bd):
+    # ESTABLECEMOS LA QUERY
+    query = TOTAL_VENDEDORES
+    # EJECUTAMOS LA QUERY
+    with bd.cursor() as cursor:
+        cursor.execute(query)
+        columnas = [columna[0] for columna in cursor.description]
+        tabla = PrettyTable(columnas)
+        registros = cursor.fetchall()
+        # AGREGAMOS LOS REGISTROS A LA TABLA
+        for registro in registros:
+            tabla.add_row(registro)
+        # IMPRIMIMOS LA TABLA
+        print(tabla)
+
+def terceraFuncion(bd):
+    usuario_id = seleccionarUsuario()
+    # ESTABLECEMOS LA QUERY
+    query = f'''SELECT venta.nombre as nombre_venta, vendedor.nombre as nombre_vendedor
+    FROM venta
+    INNER JOIN vendedor ON venta.id_vendedor = vendedor.id
+    WHERE venta.id_vendedor = {usuario_id}'''
+    # EJECUTAMOS LA QUERY
+    with bd.cursor() as cursor:
+        cursor.execute(query)
+        columnas = [columna[0] for columna in cursor.description]
+        tabla = PrettyTable(columnas)
+        registros = cursor.fetchall()
+        # AGREGAMOS LOS REGISTROS A LA TABLA
+        for registro in registros:
+            tabla.add_row(registro)
+        # IMPRIMIMOS LA TABLA
+        print(tabla)
+
+def seleccionarUsuario():
+    inp = input(INPUT_ID_USER)
+    return inp
