9131510dc2b53594984b107fb276a1a4b66773c6
María Rosa Morales <69001260+mariarosa17@users.noreply.github.com>
2023-06-26 23:20:54 -0300

    Update estudiantes.sql

--- Java/semana11/estudiantes.sql
+++ Java/semana11/estudiantes.sql
@@ -3,3 +3,7 @@ INSERT INTO estudiantes2022 (nombre, apellido, email, telefono) VALUES ('jose', 
 UPDATE estudiantes2022 SET nombre = 'juan carlos', apellido = 'garcia' WHERE id = 1;
 DELETE FROM estudiantes2022 WHERE id = 3;
 ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;
+INSERT INTO estudiantes2022 (nombre, apellido, email, telefono) VALUES ('julian', 'perez', 'juli.pe@gmail.com', '123456789');
+INSERT INTO estudiantes2022 (nombre, apellido, email, telefono) VALUES ('maria', 'lopez', 'marialopez@gmail.com', '789456456');
+INSERT INTO estudiantes2022 (nombre, apellido, email, telefono) VALUES ('jose', 'luna', 'joseluna@gmail.com', '56478151');
+
